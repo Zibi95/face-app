c9b26df2933443383b1daf4f8bbb2ea91a2a44d3
Zibi95 <sylwester.sob95@gmail.com>
2023-01-22 20:12:33 +0100

    init react app, add tailwindcss, add components

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <nav className="flex justify-end m-6">
+      <span className="text-2xl underline cursor-pointer transition-all duration-500 hover:text-gray-600 hover:no-underline">
+        Sign out
+      </span>
+    </nav>
+  );
+};
+
+export default Navbar;
