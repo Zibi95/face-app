46a69bd877fceddf6e4ec2789b47d75fe019cda6
Zibi95 <sylwester.sob95@gmail.com>
2023-02-06 12:48:17 +0100

    create new helper functions for Main components

--- src/routes/main/Main.js
+++ src/routes/main/Main.js
@@ -1,48 +1,14 @@
 import React, { useEffect, useState } from 'react';
+// Helper functions
+import {
+  fetchClarifaiFaceDetection,
+  calculateFaceLocation,
+} from './main.helper';
+// Components
 import Form from '../../components/Form';
 import DetectionImage from '../../components/DetectionImage';
+// Style
 import '../../App.css';
-
-function calculateFaceLocation(data, setBox) {
-  const image = document.getElementById('inputImage');
-  const width = Number(image.width);
-  const height = Number(image.height);
-
-  const clarifaiFaces = data.outputs[0].data.regions?.map(region => region);
-
-  clarifaiFaces.forEach(region => {
-    const { top_row, right_col, bottom_row, left_col } =
-      region.region_info.bounding_box;
-
-    setBox(prevState => [
-      ...prevState,
-      {
-        leftCol: left_col * width,
-        topRow: top_row * height,
-        rightCol: width - right_col * width,
-        bottomRow: height - bottom_row * height,
-      },
-    ]);
-  });
-}
-
-async function fetchClarifaiFaceDetection(imageUrl) {
-  const postOptions = {
-    method: 'post',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ imageUrl }),
-  };
-  try {
-    const response = await fetch('http://localhost:3000/imageurl', postOptions);
-    const data = await response.json();
-
-    return data;
-  } catch (err) {
-    console.log(err);
-  }
-}
 
 function Main({ user }) {
   const [imageUrl, setImageUrl] = useState('');
---
+++ src/routes/main/main.helper.js
@@ -0,0 +1,42 @@
+//Function that fetch from backend API call to clarifai face-detection model
+export async function fetchClarifaiFaceDetection(imageUrl) {
+  const postOptions = {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ imageUrl }),
+  };
+  try {
+    const response = await fetch('http://localhost:3000/imageurl', postOptions);
+    const data = await response.json();
+
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+// Function that calculate face location
+export function calculateFaceLocation(data, setBox) {
+  const image = document.getElementById('inputImage');
+  const width = Number(image.width);
+  const height = Number(image.height);
+
+  const clarifaiFaces = data.outputs[0].data.regions?.map(region => region);
+
+  clarifaiFaces.forEach(region => {
+    const { top_row, right_col, bottom_row, left_col } =
+      region.region_info.bounding_box;
+
+    setBox(prevState => [
+      ...prevState,
+      {
+        leftCol: left_col * width,
+        topRow: top_row * height,
+        rightCol: width - right_col * width,
+        bottomRow: height - bottom_row * height,
+      },
+    ]);
+  });
+}
